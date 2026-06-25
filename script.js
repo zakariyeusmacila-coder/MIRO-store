2ba29d82ecec42a8499aa460245c3bd0270233c1
zakariyeusmacila-coder <zakariyeusmacila@gmail.com>
2026-06-25 15:08:11 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Miro Store website-ku waa diyaar!");
+
+// Halkan ayaad mustaqbalka ku dari kartaa alaabta si ay u soo baxaan si toos ah
+window.onload = function() {
+    console.log("Xogtaada waa mid la rakibay.");
+};
